2c36166f504880ae1a02ad5973c652d19fa62be3
roman.kirpa <ghostdrs@gmail.com>
2020-06-05 13:54:43 +0300

    added food

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public BoxCollider2D food;
+
+    void Start()
+    {
+        food = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var random = new System.Random();
+        int x = random.Next(0, 10);
+        int y = random.Next(0, 10);
+        food.transform.position = new Vector2((float)x, (float)y);
+    }
+
+}
